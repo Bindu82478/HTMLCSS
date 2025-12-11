1237265f2d3f4facc0cc81979a32e70296046428
Bindu82478 <bindu82478@gmail.com>
2025-12-11 13:36:19 +0530

    [updated js files]

---
+++ function.js
@@ -0,0 +1,21 @@
+var x=12;
+var y=18;
+var z=12;
+var result=x+y+z;
+console.log(result);
+
+//function definition
+function f1(){
+    var x=12;
+var y=18;
+var z=12;
+var result=x+y+z;
+console.log(result);
+}
+
+//to call function to tell to js engien to exceute function we have to call
+f1();
+f1();
+f1();
+f1();
+f1();
---
+++ variables.js
@@ -0,0 +1,19 @@
+var abc;
+//if we defined abc with nov value it will assign abc to the value undefecined as we dint assign value to abc in line 1
+//so it will store value as undefined to abc  vaeraibale name  the value is undefined  
+//here it will name abc to the memorylocation to the location where it store 123
+//abc is teh name we have given to memory location where 123 stored
+var abc=123;
+console.log(abc);
+var x=12;
+var y=18;
+var z=12;
+var result=x+y+z;
+console.log(result);
+var x=12;
+var y=18;
+var z=12;
+var result=x+y+z;
+console.log(result);
+var abc=null;
+console.log(abc);
